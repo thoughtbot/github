d9100cbbbd291e6bfd7718cff9598fc43a5f4c69
Joe Ferris <jferris@thoughtbot.com>
2014-12-11 13:54:28 -0500

    Implement PUT /teams/:id/memberships/:username

--- Github/Organizations/Teams.hs
+++ Github/Organizations/Teams.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- | The organization teams API as described on
 -- <https://developer.github.com/v3/orgs/teams/>
 module Github.Organizations.Teams (
- userTeams
+ addUserToTeam
+,userTeams
 ,module Github.Data
 ) where
 
 import Github.Data
 import Github.Private
+import Network.HTTP.Types (Status)
+
+addUserToTeam :: GithubAuth -> Int -> String -> IO (Either Error Status)
+addUserToTeam auth teamId username =
+    doHttpsStatus "PUT" (buildUrl ["teams", show teamId, "memberships", username]) auth Nothing
 
 userTeams :: GithubAuth -> IO (Either Error [GithubTeam])
 userTeams auth = githubGet' (Just auth) ["user", "teams"]
